546b765baf26765be257f9960a8b1d8e4939320a
Maykol Marquez <101759860+maykol1801@users.noreply.github.com>
2024-11-17 09:35:28 -0600

    Initial commit

---
+++ tienda_virtual/Controllers/Home.php
@@ -0,0 +1,29 @@
+<?php 
+	require_once("Models/TCategoria.php");
+	require_once("Models/TProducto.php");
+	class Home extends Controllers{
+		public $views;
+		public $model;
+
+		use TCategoria, TProducto;
+		public function __construct()
+		{
+			parent::__construct();
+			session_start();
+		}
+
+		public function home()
+		{
+			$pageContent = getPageRout('inicio');
+			$data['page_tag'] = NOMBRE_EMPESA;
+			$data['page_title'] = NOMBRE_EMPESA;
+			$data['page_name'] = "tienda_virtual";
+			$data['page'] = $pageContent;
+			$data['slider'] = $this->getCategoriasT(CAT_SLIDER);
+			$data['banner'] = $this->getCategoriasT(CAT_BANNER);
+			$data['productos'] = $this->getProductosT();
+			$this->views->getView($this,"home",$data); 
+		}
+
+	}
+ ?>
